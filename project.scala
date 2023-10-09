50b4d7983f574226e2c1a2ff1da5c7ca06bacb8f
wahtique <williamvphan@yahoo.fr>
2023-10-09 14:54:26 +0200

    fix: tests
    worked in vscode but not from command line

--- project.scala
+++ project.scala
@@ -1,5 +1,4 @@
 //> using scala 3.3.1
 //> using toolkit typelevel:latest
-//> using testFramework munit
 //
 // TODO template this
--- src/test/ lib/Lib.test.scala
+++ src/test/ lib/Lib.test.scala
@@ -1,5 +1,7 @@
 package starter.lib
 
-class LibTest extends munit.FunSuite:
+import munit.CatsEffectSuite
+
+class LibTest extends CatsEffectSuite:
   test("Say hello to the world"):
     assertEquals(Lib.hello("World"), "Hello World!")
