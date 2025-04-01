8b3bf239782494f85920062ac9c4e5c4774fa2bf
wahtique <wvealphan@netwo.com>
2025-04-01 15:04:56 +0200

    cleanup: no need to have multiple functions

--- src/test/ lib/InverseMaths.test.scala
+++
@@ -1,8 +0,0 @@
-package starter.lib
-
-class InverseMathsTest extends munit.FunSuite:
-  test("2 3 + retourne 5"):
-    assertEquals(InverseMaths.parse("2 3 +"), 5)
-
-  test("8 4 3 + - 5 + retourne 6"):
-    assertEquals(InverseMaths.parse("8 4 3 + - 5 +"), 6)
